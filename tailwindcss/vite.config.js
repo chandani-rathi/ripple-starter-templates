74bdf5cc30652623571cec336754da7f38e363ff
Chadni Rathi <chandani.rathi@gmail.com>
2025-09-10 02:47:30 +0530

    added shadcn ui support in tailwindcss project

--- tailwindcss/vite.config.js
+++ tailwindcss/vite.config.js
@@ -1,9 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { ripple } from 'vite-plugin-ripple';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [ripple(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	server: {
 		port: 3000,
 	},
