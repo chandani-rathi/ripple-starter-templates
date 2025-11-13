f707776ba28c71b53e8421a33c66bc17317f4115
Chadni Rathi <chandani.rathi@gmail.com>
2025-11-13 23:41:24 +0530

    fixed test cases in ripple-lib

--- templates/ripple-lib/packages/ripple-lib/vitest.config.js
+++ templates/ripple-lib/packages/ripple-lib/vitest.config.js
@@ -5,6 +5,7 @@ import path from 'node:path';
 export default defineConfig({
 	plugins: [ripple({})],
 	resolve: {
+		conditions: process.env.VITEST ? ['browser'] : undefined,
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
